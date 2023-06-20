ea40ce4c996d90697678fe2ff38df312cfd8af7b
Calamatitum <46720391+Calamatitum@users.noreply.github.com>
2023-06-20 16:31:50 +0200

    List component 1

---
+++ src/People/LargePersonListItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const LargePersonListItem = ({person}) => {
+  const { name, age, hairColor, hobbies } = person;
+  return (
+    <>
+      <h3></h3>
+    <>
+  )
+}
